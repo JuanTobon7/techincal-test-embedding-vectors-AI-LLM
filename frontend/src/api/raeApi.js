282dc362addeef23c14b1ac12c7d2218f4bc1a53
Juan_Tobon <jctobon11.2@gmail.com>
2026-02-17 10:44:46 -0500

    update baseURL in raeApi.js to point to the correct API endpoint

--- frontend/src/api/raeApi.js
+++ frontend/src/api/raeApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://juanchodev.pythonanywhere.com/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://techincal-test-embedding-vectors-ai-llm.onrender.com/",
   timeout: 15000,
 });
 
